7152e5817a29004ceb6afa1811aa78e2c0a765f9
dogfryd <dogfryd@gmail.com>
2025-03-03 00:50:16 -0800

    began sound making
    added a sound controller, 2 sounds, and added a sound to item pickups

--- Assets/Scenes/Scripts/Inventory/ItemPickable.cs
+++ Assets/Scenes/Scripts/Inventory/ItemPickable.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class ItemPickable : MonoBehaviour, IPickable
 {
     public InventoryItemData itemScriptableObject;
-
+    [SerializeField] private AudioClip pickUpSound;
     public void PickItem()
     {
+        SoundFXManager.instance.PlaySoundFXClip(pickUpSound, transform, 1f);
         Destroy(gameObject);
     }
 }
---
+++ Assets/Scenes/Scripts/SoundScripts/SoundFXManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundFXManager : MonoBehaviour
+{
+
+    public static SoundFXManager instance;
+    [SerializeField] private AudioSource soundFXObject;
+    private void Awake()
+    {
+        if (instance == null) 
+        {
+            instance = this;
+        }   
+    }
+
+    public void PlaySoundFXClip (AudioClip audioClip, Transform spawnTransform, float volume)
+    {
+        //spawn in gameobject
+        AudioSource audioSource = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
+        
+        //assign audioclip
+        audioSource.clip = audioClip;
+
+        //assign volume
+        audioSource.volume = volume;
+
+        //play sound
+        audioSource.Play();
+
+        //get length of soundclip
+        float clipLength = audioSource.clip.length;
+
+        //destroy clip after length is finished
+        Destroy(audioSource.gameObject, clipLength);
+    }
+
+}
