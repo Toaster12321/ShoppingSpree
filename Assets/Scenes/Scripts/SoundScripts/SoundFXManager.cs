3fb9dc29dba1dd627dba6485642c8ac033977ac9
Toaster12321 <tonsterrocks123@gmail.com>
2025-03-08 14:41:56 -0800

    Volume Slider and GameManagers
    added a volume slider in the pause menu that works with the SoundFXManager script
    also added a global game manager that houses any other managers we make

--- Assets/Scenes/Scripts/SoundScripts/SoundFXManager.cs
+++ Assets/Scenes/Scripts/SoundScripts/SoundFXManager.cs
@@ -1,9 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Schema;
 using UnityEngine;
 
-public class SoundFXManager : MonoBehaviour
+public class SoundFXManager : MonoBehaviour, IGameManager
 {
+
+    //setter/getter function to be referenced for the volume slider in the settings script
+    public float soundVolume
+    {
+        get { return AudioListener.volume; }
+        set { AudioListener.volume = value; }
+    }
+    //setter/ getter for the manager status script
+    public ManagerStatus status { get; private set; }
+    //startup function needed to use IGameManager interface script
+    public void Startup()
+    {
+        Debug.Log("Audio Manager starting...");
+
+        //soundVolume acts as the global sound source
+        soundVolume = 0.5f;
+
+        status = ManagerStatus.Started;
+    }
 
     public static SoundFXManager instance;
     [SerializeField] private AudioSource soundFXObject;
@@ -27,7 +47,7 @@ public class SoundFXManager : MonoBehaviour
         audioSource.clip = audioClip[rand];
 
         //assign volume
-        audioSource.volume = volume;
+        audioSource.volume = soundVolume;
 
         //play sound
         audioSource.Play();
@@ -48,7 +68,7 @@ public class SoundFXManager : MonoBehaviour
         audioSource.clip = audioClip;
 
         //assign volume
-        audioSource.volume = volume;
+        audioSource.volume = soundVolume;
 
         //play sound
         audioSource.Play();
