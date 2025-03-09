04099e46124396d1fdbcd732ff6f4c016f1cae42
Toaster12321 <tonsterrocks123@gmail.com>
2025-03-08 16:34:22 -0800

    Revert "Volume Slider and Game Manager"
    This reverts commit 1d60ca272705dc5f280dbc19b6575c85db784f21.

--- Assets/Scenes/Scripts/SoundScripts/SoundFXManager.cs
+++ Assets/Scenes/Scripts/SoundScripts/SoundFXManager.cs
@@ -1,53 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Xml.Schema;
 using UnityEngine;
 
-public class SoundFXManager : MonoBehaviour, IGameManager
+public class SoundFXManager : MonoBehaviour
 {
-
-    //setter/getter function to be referenced for the volume slider in the settings script
-    public float soundVolume
-    {
-        get { return AudioListener.volume; }
-        set { AudioListener.volume = value; }
-    }
-    //setter/ getter for the manager status script
-    public ManagerStatus status { get; private set; }
-    //startup function needed to use IGameManager interface script
-    public void Startup()
-    {
-        Debug.Log("Audio Manager starting...");
-
-        //soundVolume acts as the global sound source
-        soundVolume = 0.5f;
-
-        status = ManagerStatus.Started;
-    }
 
     public static SoundFXManager instance;
     [SerializeField] private AudioSource soundFXObject;
     private void Awake()
     {
-        if (instance == null)
+        if (instance == null) 
         {
             instance = this;
-        }
+        }   
     }
 
-    public void PlayRandomSoundFXClip(AudioClip[] audioClip, Transform spawnTransform, float volume)
+    public void PlayRandomSoundFXClip (AudioClip[] audioClip, Transform spawnTransform, float volume)
     {
         //randomize
         int rand = Random.Range(0, audioClip.Length);
 
         //spawn in gameobject
         AudioSource audioSource = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
-
+        
         //assign audioclip
         audioSource.clip = audioClip[rand];
 
         //assign volume
-        audioSource.volume = soundVolume;
+        audioSource.volume = volume;
 
         //play sound
         audioSource.Play();
@@ -59,16 +39,16 @@ public class SoundFXManager : MonoBehaviour, IGameManager
         Destroy(audioSource.gameObject, clipLength);
     }
 
-    public void PlaySoundFXClip(AudioClip audioClip, Transform spawnTransform, float volume)
+    public void PlaySoundFXClip (AudioClip audioClip, Transform spawnTransform, float volume)
     {
         //spawn in gameobject
         AudioSource audioSource = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
-
+        
         //assign audioclip
         audioSource.clip = audioClip;
 
         //assign volume
-        audioSource.volume = soundVolume;
+        audioSource.volume = volume;
 
         //play sound
         audioSource.Play();
