b675b2812a4332ffe1b2a4099b58355210e89d58
Clahm21 <clahm21@gmail.com>
2025-05-13 14:18:05 -0700

    Attmpt 1 Push Connor
    Ik it wont do what i want first try, this is main menu updated w modes and spawning system

--- Assets/Scripts/HUD/MainMenu.cs
+++ Assets/Scripts/HUD/MainMenu.cs
@@ -2,15 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject waveSliderPanel;
+    public Slider waveSlider;
+    public TMPro.TMP_Text waveSliderText;
+
     public void Start()
     {
         Cursor.lockState = CursorLockMode.None;
+<<<<<<< Updated upstream
+=======
+        Cursor.visible = true;
+        if(waveSliderPanel != null)
+        {
+            waveSliderPanel.SetActive(false);
+        }
+>>>>>>> Stashed changes
     }
-    public void PlayGame()
+
+    public void PlayEndlessGame()
     {
+        GameManager.selectedGameMode = GameMode.Endless;
+        SceneManager.LoadSceneAsync(1);
+    }
+
+public void PlaySurvivalGame()
+    {
+        GameManager.selectedGameMode = GameMode.Survival;
+        // Set the wave slider panel active b4 loading the scene
+        if (waveSliderPanel != null)
+        {
+            waveSliderPanel.SetActive(true);
+        }
+     
+    }
+
+//method when changing the slider value
+    public void OnWaveSliderValueChanged(float value)
+    {
+        if (waveSliderText != null && waveSliderPanel != null)
+        {
+            waveSliderText.text = value.ToString("0");
+        }
+    }
+
+    public void StartSurvivalGame()
+    {
+        if (waveSlider != null)
+        {
+            GameManager.WavesToRun = Mathf.RoundToInt(waveSlider.value);
+        }
         SceneManager.LoadSceneAsync(1);
     }
 
