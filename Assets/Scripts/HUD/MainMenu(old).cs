3866a99d7e8dec96194f88eef5fef7a4ef38f8ed
Juan Enriquez <juan03192001@gmail.com>
2025-05-14 16:21:58 -0700

    Working Now

--- Assets/Scripts/HUD/MainMenu(old).cs
+++ Assets/Scripts/HUD/MainMenu(old).cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class MainMenu : MonoBehaviour
+public class MainMenuOld : MonoBehaviour
 {
     public GameObject waveSliderPanel;
     public Slider waveSlider;
