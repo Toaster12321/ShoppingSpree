c47ddcac4c431a01123aa6db4a63c4deda01f1f3
Juan Enriquez <juan03192001@gmail.com>
2025-05-13 22:16:07 -0700

    Jay big dih

---
+++ Assets/Juan's Stuff/Scripts/Simple_Text_PopUp.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Simple_Text_PopUp : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
